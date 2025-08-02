dbf54cce140df3b3b1e835eba75fcd1a3a6f9e3e
abhiram <abhi18.b@gmail.com>
2025-08-02 19:09:49 +0530

    component scan and use of scanBasePackages in SpringBootApplication annotation

---
+++ 02-spring-boot-spring-core/02-component-scan/src/main/java/com/example/springcore/SpringCoreApplication.java
@@ -0,0 +1,16 @@
+package com.example.springcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication(
+//		scanBasePackages = {"com.example.springcore","com.example.util"}
+//)
+@SpringBootApplication
+public class SpringCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreApplication.class, args);
+	}
+
+}
