ee12228144c64c4f19b18785ce5ff5ccbda9c3b4
abhiram <abhi18.b@gmail.com>
2025-08-03 13:10:41 +0530

    Created a basic commandLine application and established connection with mysql database

---
+++ 03-spring-hibernate-jpa-crud/crudapplication/src/main/java/com/example/crudapplication/CrudApplication.java
@@ -0,0 +1,24 @@
+package com.example.crudapplication;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		return (runner)->{
+			System.out.println("Hello World!");
+		};
+	}
+
+}
