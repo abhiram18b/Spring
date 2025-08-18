11db51d7160d4cd7c0f8fd88d930863ac72a9d2e
abhiram <abhi18.b@gmail.com>
2025-08-18 21:52:46 +0530

    Adding data to model

--- 06-spring-boot-mvc/thymeleafdemo/src/main/java/com/example/springmvc/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-mvc/thymeleafdemo/src/main/java/com/example/springmvc/thymeleafdemo/controller/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.example.springmvc.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -13,6 +16,24 @@ public class HelloWorldController {
 
     @RequestMapping("/processForm")
     public String processForm(){
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersion2")
+    public String formDataToAllCaps(HttpServletRequest request, Model model){
+        String theName = request.getParameter("studentName");
+        theName = theName.toUpperCase();
+
+        String result= "Yo!"+theName;
+        model.addAttribute("message",result);
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersion3")
+    public String processFormVersionThree(@RequestParam("studentName")String theName,Model model){
+        theName = theName.toUpperCase();
+        String result = "Hey my friend"+theName;
+        model.addAttribute("message",result);
         return "helloworld";
     }
 }
