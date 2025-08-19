a9291723f38d2d69e64e5441380b4e2eddb812d0
abhiram <abhi18.b@gmail.com>
2025-08-19 23:22:50 +0530

    read custom properties for checkboxes from properties file

--- 06-spring-boot-mvc/thymeleafdemofields/src/main/java/com/example/springmvc/thymeleafdemo/controller/StudentFormController.java
+++ 06-spring-boot-mvc/thymeleafdemofields/src/main/java/com/example/springmvc/thymeleafdemo/controller/StudentFormController.java
@@ -19,12 +19,16 @@ public class StudentFormController {
     @Value("${languages}")
     private List<String> languages;
 
+    @Value("${systems}")
+    private List<String> systems;
+
     @RequestMapping("/showStudentForm")
     public String showStudentForm(Model theModel){
         Student theStudent = new Student();
         theModel.addAttribute("student",theStudent);
         theModel.addAttribute("countries",countries);
         theModel.addAttribute("languages",languages);
+        theModel.addAttribute("systems",systems);
         return "student-form";
     }
 
