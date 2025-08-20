f61f0ad112f4affdb460c9d06b7336787b8dfd0d
abhiram <abhi18.b@gmail.com>
2025-08-21 00:36:43 +0530

    spring mvc validation

---
+++ 06-spring-boot-mvc/validationdemo/src/main/java/com/example/springboot/mvc/validationdemo/CustomerController.java
@@ -0,0 +1,39 @@
+package com.example.springboot.mvc.validationdemo;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CustomerController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+        StringTrimmerEditor trimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class,trimmerEditor);
+    }
+
+    @GetMapping("/showForm")
+    public String showForm(Model theModel){
+        theModel.addAttribute("customer",new Customer());
+        return "customer-form";
+    }
+
+    @PostMapping("/processForm")
+    public String processForm(@Valid @ModelAttribute("customer")Customer customer, BindingResult bindingResult){
+
+        System.out.println("LastName |"+customer.getLastName()+"|");
+        System.out.println(bindingResult.toString());
+        if(bindingResult.hasErrors()){
+            return "customer-form";
+        }
+        return "customer-confirmation";
+    }
+}
